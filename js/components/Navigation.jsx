7fb9af55db261be44fb593e30a99788910446541
Maggie Love <maggie.love1991@gmail.com>
2024-05-03 16:33:33 -0400

    Adding responsive styles

--- js/components/Navigation.jsx
+++ js/components/Navigation.jsx
@@ -38,23 +38,34 @@ const Navigation = ({ updateFilms, onSignIn, token, username }) => {
   }
 
    return (
-     <div className="navigation">
-      <div className="left">
-        <h1>The Criteria Channel</h1>
-        <a href="/">Now Playing</a>
-        <a href="#allFilms">All Films</a>
-        <form onKeyDown={handleKeyDown} onSubmit={handleSubmit}>
-          <label htmlFor="search" onClick={handleSearchClick}>Search</label>
-          <input ref={inputRef} type="text" id="search" value={searchTerm}
-            onChange={handleChange} />
-        </form>
-      </div>
+     <>
+       <nav className="navigation desktop">
+        <div className="left">
+          <h1 className="logo">The Criteria Channel</h1>
+          <a href="/">Now Playing</a>
+          <a href="#allFilms">All Films</a>
+          <form onKeyDown={handleKeyDown} onSubmit={handleSubmit}>
+            <label htmlFor="search" onClick={handleSearchClick}>Search</label>
+            <input ref={inputRef} type="text" id="search" value={searchTerm}
+              onChange={handleChange} />
+          </form>
+        </div>
 
-      <div className="right">
-        {!token ? <p className="sign-in" onClick={onSignIn}>Sign in</p>
-          : <div><p>Hi, {token.username}</p></div>}
-      </div>
-     </div>
+        <div className="right">
+          {!token ? <p className="sign-in" onClick={onSignIn}>Sign in</p>
+            : <div><p>Hi, {token.username}</p></div>}
+        </div>
+       </nav>
+
+       <nav className="navigation mobile">
+          <svg className="hamburger-icon" xmlns="http://www.w3.org/2000/svg" width="50" height="50"
+          viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2"
+          stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu hamburger-icon">
+          <line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21"
+          y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
+         <h1 className="logo">The Criteria Channel</h1>
+       </nav>
+     </>
    );
 };
 
--- js/components/Slides.jsx
+++ js/components/Slides.jsx
@@ -1,50 +1,47 @@
 import { Slide } from 'react-slideshow-image';
-// import 'react-slideshow-image/dist/styles.css';
-
-const spanStyle = {
-  padding: '20px',
-  background: 'none',
-  color: '#000000'
-}
 
 const divStyle = {
-  display: 'flex',
-  padding: '20px',
-  alignItems: 'center',
-  justifyContent: 'center',
-  backgroundSize: '100%',
-  minHeight: '670px',
-  height: '100%'
+  // display: 'flex',
+  // padding: '20px',
+  // alignItems: 'center',
+  // justifyContent: 'center',
+  // backgroundSize: '100%',
+  // minHeight: '670px',
+  // height: '100%'
 }
 
 const slideImages = [
   {
     url: 'https://vhx.imgix.net/criterionchannelchartersu/assets/bd5a405b-f24d-43a6-a834-471076bb30d1.jpg?auto=format%2Ccompress&fit=crop&h=720&q=75&w=1280',
-    caption: ''
+    alt: `One Night: featuring Collateral, Assault on Precinct 13, After Hours,
+    Before Sunrise, Mikey and Nicky, and more.`
   },
   {
     url: 'https://vhx.imgix.net/criterionchannelchartersu/assets/02d18272-1ade-478f-b908-d62201ebe38a.jpg?auto=format%2Ccompress&fit=crop&h=720&q=75&w=1280',
-    caption: ''
+    alt: `1950 Peak Noir: featuring Sunset Boulevard, Gun Crazy, In a Lonely Place,
+    The Asphalt Jungle, Night and the City, and more. With an introduction by Imogen
+    Sara Smith and Eddie Muller.`
   },
   {
     url: 'https://vhx.imgix.net/criterionchannelchartersu/assets/ff4dc209-4d32-42a9-a02e-1187cc23078d.jpg?auto=format%2Ccompress&fit=crop&h=720&q=75&w=1280',
-    caption: ''
+    alt: `Celebrate Black History: Featuring Daughters of the Dust, After Sherman,
+    Tongues Untied, Black Panthers`
   },
   {
     url: 'https://vhx.imgix.net/criterionchannelchartersu/assets/a863edc3-9ab8-40dc-8a79-7ea8ce4312a6.jpg?auto=format%2Ccompress&fit=crop&h=720&q=75&w=1280',
-    caption: ''
+    alt: `Living the Part: featuring performances by Robert De Niro, Meryl Streep,
+    Joaquin Phoenix, Cate Blanchett, Jefrrey Wright, and more. With an introduction
+    by Isaac Butler.`
   },
 ];
 
 const Slides = () => {
     return (
       <div className="slide-container">
-        <Slide autoplay={false}>
+        <Slide autoplay={false} indicators={true}>
          {slideImages.map((slideImage, index)=> (
-            <div key={index}>
-              <div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
-                <span style={spanStyle}>{slideImage.caption}</span>
-              </div>
+            <div key={index} style={{ ...divStyle }}>
+              <img src={slideImage.url} alt={slideImage.alt} />
             </div>
           ))}
         </Slide>
